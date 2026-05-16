dfd09aa4fdde4f112dcb0da42bb683e42f0a3618
igorsatsyuk <igor.satsyuk2@gmail.com>
2026-05-16 20:34:29 +0300

    Remove unused import from BlockchainWriterServiceTest

--- backend/audit-writer-service/src/test/java/lt/satsyuk/distributed/audit/auditwriter/service/BlockchainWriterServiceTest.java
+++ backend/audit-writer-service/src/test/java/lt/satsyuk/distributed/audit/auditwriter/service/BlockchainWriterServiceTest.java
@@ -21,7 +21,6 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import java.math.BigInteger;
 import java.time.Instant;
 import java.util.concurrent.Callable;
-import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.Optional;
